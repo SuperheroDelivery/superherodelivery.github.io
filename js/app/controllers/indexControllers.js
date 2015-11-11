ff60c18803d7417ee1b1e286949c354001d56c3d
opuerto <oscar.puertohn@gmail.com>
2015-11-11 11:41:31 +0000

    contact form

--- js/app/controllers/indexControllers.js
+++ js/app/controllers/indexControllers.js
@@ -2,6 +2,12 @@ superheroApp.controller('formCode', ['$scope',  '$http',
     function($scope, $http) {
           $scope.zipcode = '';
 
+          $scope.name = "";
+          $scope.email = "";
+          $scope.phone = "";
+          $scope.message = ""; 
+          $scope.subject = "";
+
           $scope.goToRestaurants = function ()
           {
             //window.location.replace("http://superherodelivery/restaurants.html");
@@ -15,6 +21,39 @@ superheroApp.controller('formCode', ['$scope',  '$http',
                         console.log(response);
                         swal({   title: "Error!",   text: response.data,   type: "error",   confirmButtonText: "Try another" });
                 });
+          }
+
+          $scope.validate = function()
+          {
+              if ($scope.name == "" ||  $scope.email == "" || $scope.phone == "" || $scope.message == "" || $scope.subject =="" ) 
+              {
+                 
+                 return false; 
+                  
+              }else
+              {
+                return true;
+              }
+              
+          }
+
+          $scope.contactForm = function()
+          {
+            
+            if ($scope.validate()) 
+              {
+                swal({   title: "We got your message!!!",   text: "Thanks for your message",   type: "success",   confirmButtonText: "Send another" });
+                 $scope.name = "";
+          $scope.email = "";
+          $scope.phone = "";
+          $scope.message = ""; 
+          $scope.subject = "";
+              }
+            else
+            {
+              swal({   title: "Error!",   text: "Complete the information on the form before send a message",   type: "error",   confirmButtonText: "Try again" });
+            }
+
           }
 
 
